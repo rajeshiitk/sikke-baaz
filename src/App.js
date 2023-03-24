6c2269a948d3c3487b50d4337a235f31f0993dbc
Rajesh Choudhary <rajuvert18@gmail.com>
2023-03-24 16:09:54 +0530

    home

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={ <Home />}/>
+        <Route path="/sikke-baaz" element={ <Home />}/>
         <Route path="/sikke-baaz/coins" element={ <Coins />}/>
         <Route path="/sikke-baaz/exchanges" element={ <Exchanges />}/>
         <Route path="/sikke-baaz/coin/:id" element={ <CoinDetails />}/>
